f65be078075667ffef0c5096559311cb97eb1c91
ansh821 <anshikachauhan7658@gmail.com>
2026-09-02 18:09:28 +0530

    Configured Snowflake warehouse and database structure

--- backend/database.py
+++ backend/database.py
@@ -16,7 +16,7 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
-    bind=engineglobaluser.name
+    bind=engine
 )
 
 Base = declarative_base()
